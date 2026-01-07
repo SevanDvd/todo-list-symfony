0f33a91401b5187fc9c33e689c0b39ced92d8040
SevanDvd <sevan.vardanian@epitech.eu>
2026-01-07 16:58:39 +0100

    init

---
+++ src/Enum/TaskPriority.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+enum TaskPriority: string
+{
+    case LOW = 'low';
+    case MEDIUM = 'medium';
+    case HIGH = 'high';
+}
+
+?>
---
+++ src/Enum/TaskType.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Enum;
+
+enum TaskType: string
+{
+    case COOKING = 'cooking';
+    case TRAINING = 'training';
+    case CLEANING = 'cleaning';
+}
+
+?>
